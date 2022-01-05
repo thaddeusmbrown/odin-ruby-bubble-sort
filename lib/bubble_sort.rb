3a48a2496d564f82130fb22516e90e5b8930764f
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-04 20:30:52 -0600

    set up project and test environment.  Wrote tests for bubble sort method.

---
+++ lib/bubble_sort.rb
@@ -0,0 +1,5 @@
+require 'pry-byebug'
+
+def bubble_sort(array)
+  # sort an array with bubble sort method
+end
---
+++ spec/bubble_sort_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require_relative '../lib/bubble_sort'
+
+RSpec.describe 'BubbleSort' do
+  describe 'sorts arrays with bubble sort method' do
+    it 'can sort when one number is out of order' do
+      array = [1, 5, 2, 3, 4]
+      result = [1, 2, 3, 4, 5]
+      expect(bubble_sort(array)).to eq(result)
+    end
+
+    xit 'can sort when two numbers are out of order' do
+      array = [1, 5, 2, 4, 3]
+      result = [1, 2, 3, 4, 5]
+      expect(bubble_sort(array)).to eq(result)
+    end
+
+    xit 'can sort worst case of descending order' do
+      array = [5, 4, 3, 2, 1]
+      result = [1, 2, 3, 4, 5]
+      expect(bubble_sort(array)).to eq(result)
+    end
+
+    xit 'returns correct result when array already sorted' do
+      array = [1, 2, 3, 4, 5]
+      result = [1, 2, 3, 4, 5]
+      expect(bubble_sort(array)).to eq(result)
+    end
+  end
+end
