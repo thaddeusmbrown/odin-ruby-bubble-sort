1df89690911b5cde4a3a30c0de817a7788dfbc52
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-04 20:53:02 -0600

    Linted files with rubocop and cleaned up some redundant and unused variables in original solution

--- Gemfile
+++ Gemfile
@@ -3,10 +3,10 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~>3.9'
-gem 'rubocop-rspec', require: false
 
 group :development, :test do
   gem 'pry-byebug', require: false
   gem 'rubocop', require: false
   gem 'rubocop-rails', require: false
+  gem 'rubocop-rspec', require: false
 end
--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,16 +1,17 @@
-require 'pry-byebug'
+# frozen_string_literal: true
 
 def bubble_sort(array)
   # sort an array with bubble sort method
+  # iterate through the array the worst case number of times (length - 1)
+  # swap adjacent pairs if the leftmost number is greater, move left to right
 
-  # binding.pry
-
-  (0...array.length-1).each do |i|
+  (0...array.length - 1).each do
     array.each_with_index do |value, index|
-      if index < array.length - 1 && value > array[index+1]
-        array[index], array[index+1] = array[index+1], array[index]
+      if index < array.length - 1 && value > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
       end
     end
   end
+  # return final array after sorting
   array
 end
