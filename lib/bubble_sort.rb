8c9d57823ab08b8d5dfb4331b801488c1591145e
thaddeusmbrown <thaddeusmbrown@gmail.com>
2022-01-04 20:43:32 -0600

    wrote bubble_sort method and passed first test with one number out of order

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -2,4 +2,15 @@ require 'pry-byebug'
 
 def bubble_sort(array)
   # sort an array with bubble sort method
+
+  binding.pry
+
+  (0...array.length-1).each do |i|
+    array.each_with_index do |value, index|
+      if index < array.length - 1 && value > array[index+1]
+        array[index], array[index+1] = array[index+1], array[index]
+      end
+    end
+  end
+  array
 end
